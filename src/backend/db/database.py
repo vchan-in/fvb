c051d2e9d9985f104cf2fae8bc1e3a0651171aa6
Vaishno Chaitanya <17123227+vchan-in@users.noreply.github.com>
2024-02-24 01:39:05 +0530

    Add database connection retry logic

--- src/backend/db/database.py
+++ src/backend/db/database.py
@@ -1,8 +1,19 @@
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+mysqlconnector://root:password@db:3306/bank")
+# Wait and retry to connect to the database
+while True:
+    try:
+        engine = create_engine("mysql+mysqlconnector://root:password@db:3306/bank")
+        engine.connect()
+        break
+    except Exception as e:
+        print("Database not ready yet, retrying in 5 seconds")
+        time.sleep(5)
+# engine = create_engine("mysql+mysqlconnector://root:password@db:3306/bank")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 

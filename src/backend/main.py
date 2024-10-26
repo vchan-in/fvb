ca7ddb90f78fcefb0dd245154083e9563939194c
vchan-in <17123227+vchan-in@users.noreply.github.com>
2024-10-26 14:53:20 +0000

    chore: Update origins in main.py to include FVB_BACKEND_BASEURL

--- src/backend/main.py
+++ src/backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
         ],
     )
 
-origins = ['http://localhost:8080', 'http://127.0.0.1:8080']
+origins = ['http://localhost:8080', 'http://127.0.0.1:8080',FVB_BACKEND_BASEURL+':8080']
 
 app.add_middleware(
     CORSMiddleware,
